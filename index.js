aebc1d49ba4e4d281db4eee0130fc383cc930a48
Andriy Kashcha <anvaka@gmail.com>
2020-01-31 01:28:02 -0800

    Clean up

--- index.js
+++
@@ -1,4 +0,0 @@
-
-
-
-// [timeout:10][out:json];is_in(47.6188,-122.1820)->.a;way(pivot.a);out tags bb;out ids geom(47.59173471461263,-122.21560209989548,47.63223851563446,-122.17097014188766);relation(pivot.a);out tags bb;
